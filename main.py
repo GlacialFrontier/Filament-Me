42f8f6b869031ee774e819282ef319a404c87f05
GlacialFrontier <jadewsobers@gmail.com>
2026-06-23 20:41:35 -0400

    added files to store data for Projects

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+
+    # CLI title page
+    print("/n --- Weclome to Filament & Me ---")
+    print("This tool is used to track projects, Filaments and that yummy stats on both.")
+
+main()
